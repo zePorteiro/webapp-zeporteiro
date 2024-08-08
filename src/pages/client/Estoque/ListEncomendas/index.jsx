2676134ad5d995d7fd18bdbf6bd911e62ba50ee4
JuliaHown <juliahown87@gmail.com>
2024-08-08 13:50:18 -0300

    :sparkles: tabela da lista de encomendas ajustada

--- src/pages/client/Estoque/ListEncomendas/index.jsx
+++ src/pages/client/Estoque/ListEncomendas/index.jsx
@@ -1,176 +1,374 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-
+import { useMemo, useState } from 'react';
+import dayjs from 'dayjs';
 import {
-  ContainerWrapper,
-  ContentWrapper,
-  Container,
-  CabecalhoContainer,
-  TituloContainer,
-  Titulo,
-  SelecionarDataButton,
-  ImagemContainer,
-  Imagem,
-  DadosContainer,
-  LinhaCabecalho,
-  ColunaCabecalho,
-  Linha,
-  Coluna,
-  PaginacaoContainer,
-  NumeroPagina,
-  BotaoSkip,
-} from "./styles";
+  MRT_EditActionButtons,
+  MaterialReactTable,
+  // createRow,
+  useMaterialReactTable,
+} from 'material-react-table';
+import {
+  Box,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Tooltip,
+} from '@mui/material';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+import Dados from '../../../../utils/dados.json'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function TabelaEncomenda() {
-  const [encomendas, setEncomendas] = useState([]);
-  const [paginaAtual, setPaginaAtual] = useState(1);
-  const encomendasPorPagina = 5;
+const TableApartamentos = () => {
+  const [validationErrors, setValidationErrors] = useState({});
 
-  useEffect(() => {
-    const fetchEncomendas = async () => {
-      try {
-        const response = await axios.get("http://localhost:8080/entregas");
-        setEncomendas(response.data);
-      } catch (error) {
-        console.error("Erro ao buscar as encomendas:", error);
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+        enableEditing: false,
+        size: 80,
+      },
+      {
+        accessorKey: 'nome',
+        header: 'Destinatário',
+        muiEditTextFieldProps: {
+          required: true,
+          error: !!validationErrors?.nome,
+          helperText: validationErrors?.nome,
+          //remove any previous validation errors when user focuses on the input
+          onFocus: () =>
+            setValidationErrors({
+              ...validationErrors,
+              nome: undefined,
+            }),
+          //optionally add validation checking for onBlur or onChange
+        },
+      },
+      {
+        accessorKey: 'apartamento',
+        header: 'Apartamento',
+        muiEditTextFieldProps: {
+          type: 'number',
+          required: true,
+          error: !!validationErrors?.apartamento,
+          helperText: validationErrors?.apartamento,
+          //remove any previous validation errors when user focuses on the input
+          onFocus: () =>
+            setValidationErrors({
+              ...validationErrors,
+              apartamento: undefined,
+            }),
+        },
+      },
+      {
+        accessorKey: 'bloco',
+        header: 'Bloco',
+        muiEditTextFieldProps: {
+          required: false,
+          error: !!validationErrors?.bloco,
+          helperText: validationErrors?.bloco,
+          //remove any previous validation errors when user focuses on the input
+          onFocus: () =>
+            setValidationErrors({
+              ...validationErrors,
+              bloco: undefined,
+            }),
+          //optionally add validation checking for onBlur or onChange
+        },
+      },
+      {
+        accessorKey: 'dataRecebido',
+        header: 'Data Recebido',
+        muiEditTextFieldProps: {
+          required: true,
+          type: 'date',
+          error: !!validationErrors?.dataRecebido,
+          helperText: validationErrors?.dataRecebido,
+          // remove any previous validation errors when user focuses on the input
+          onFocus: () =>
+            setValidationErrors({
+              ...validationErrors,
+              dataRecebido: undefined,
+            }),
+        },
+      },
+      {
+        accessorKey: 'dataRetirado',
+        header: 'Data Retirado',
+        muiEditTextFieldProps: {
+          required: false,
+          type: 'date',
+          error: !!validationErrors?.dataRetirado,
+          helperText: validationErrors?.dataRetirado,
+          // remove any previous validation errors when user focuses on the input
+          onFocus: () =>
+            setValidationErrors({
+              ...validationErrors,
+              dataRetirado: undefined,
+            }),
+        },
+      },
+    ],
+    [validationErrors],
+  );
+
+  //call CREATE hook
+  const { mutateAsync: createUser, isPending: isCreatingUser } =
+    useCreateUser();
+  //call READ hook
+  const {
+    data: fetchedUsers = [],
+    isError: isLoadingUsersError,
+    isFetching: isFetchingUsers,
+    isLoading: isLoadingUsers,
+  } = useGetUsers();
+  //call UPDATE hook
+  const { mutateAsync: updateUser, isPending: isUpdatingUser } =
+    useUpdateUser();
+  //call DELETE hook
+  const { mutateAsync: deleteUser, isPending: isDeletingUser } =
+    useDeleteUser();
+
+  //CREATE action
+  const handleCreateUser = async ({ values, table }) => {
+    const newValidationErrors = validateUser(values);
+    if (Object.values(newValidationErrors).some((error) => error)) {
+      setValidationErrors(newValidationErrors);
+      return;
+    }
+    setValidationErrors({});
+    await createUser(values);
+    table.setCreatingRow(null); //exit creating mode
+  };
+
+  //UPDATE action
+  const handleSaveUser = async ({ values, table }) => {
+    const newValidationErrors = validateUser(values);
+    if (Object.values(newValidationErrors).some((error) => error)) {
+      setValidationErrors(newValidationErrors);
+      return;
+    }
+    setValidationErrors({});
+    await updateUser(values);
+    table.setEditingRow(null); //exit editing mode
+  };
+
+  //DELETE action
+  const openDeleteConfirmModal = (row) => {
+    if (window.confirm('Tem certeza que deseja excluir essa encomenda?')) {
+      deleteUser(row.original.id);
+    }
+  };
+
+  const table = useMaterialReactTable({
+    columns,
+    data: fetchedUsers,
+    createDisplayMode: 'modal', //default ('row', and 'custom' are also available)
+    editDisplayMode: 'modal', //default ('row', 'cell', 'table', and 'custom' are also available)
+    enableEditing: true,
+    getRowId: (row) => row.id,
+    muiToolbarAlertBannerProps: isLoadingUsersError
+      ? {
+        color: 'error',
+        children: 'Error loading data',
       }
-    };
-
-    fetchEncomendas();
-  }, []);
-
-  useEffect(() => {
-    const novaEncomenda = {
-      id: "302013",
-      destinatario: "Maria da Silva",
-      apartamento: "115",
-      bloco: "A",
-      recebido: "29/04/2024",
-      retirado: "30/04/2024",
-    };
-
-    setEncomendas((prevEncomendas) => [novaEncomenda, ...prevEncomendas]);
-  }, []);
-
-  const indiceUltimaEncomenda = paginaAtual * encomendasPorPagina;
-  const indicePrimeiraEncomenda = indiceUltimaEncomenda - encomendasPorPagina;
-  const encomendasPaginaAtual = encomendas.slice(
-    indicePrimeiraEncomenda,
-    indiceUltimaEncomenda
-  );
-
-  const mudarPagina = (numeroPagina) => {
-    setPaginaAtual(numeroPagina);
-  };
-
-  return (
-    <ContainerWrapper>
-      <ContentWrapper>
-        <Container>
-          <Cabecalho />
-          <TabelaEncomendas encomendas={encomendasPaginaAtual} />
-          <Paginacao
-            totalPaginas={Math.ceil(encomendas.length / encomendasPorPagina)}
-            paginaAtual={paginaAtual}
-            mudarPagina={mudarPagina}
-          />
-        </Container>
-      </ContentWrapper>
-    </ContainerWrapper>
-  );
-}
-
-function Cabecalho() {
-  return (
-    <CabecalhoContainer>
-      <TituloContainer>
-        <Titulo>Encomendas</Titulo>
-      </TituloContainer>
-      <SelecionarDataButton>
-        <ImagemContainer>
-          <Imagem src="https://cdn.builder.io/api/v1/image/assets/TEMP/118eaf4d0c9d7207230ff0f59dd42da874895d559bd5662fab7c4d39114cdd41?apiKey=47f1cd04243243c1a2a2819ee899bf9a&" />
-        </ImagemContainer>
-        Selecionar Data
-      </SelecionarDataButton>
-    </CabecalhoContainer>
-  );
-}
-
-function TabelaEncomendas({ encomendas }) {
-  const [linhaSelecionada, setLinhaSelecionada] = useState(null);
-
-  const selecionarLinha = (index) => {
-    setLinhaSelecionada(index);
-  };
-
-  return (
-    <DadosContainer>
-      <LinhaCabecalho>
-        <ColunaCabecalho>ID</ColunaCabecalho>
-        <ColunaCabecalho>Destinatário</ColunaCabecalho>
-        <ColunaCabecalho>Apartamento</ColunaCabecalho>
-        <ColunaCabecalho>Bloco</ColunaCabecalho>
-        <ColunaCabecalho>Data Recebido</ColunaCabecalho>
-        <ColunaCabecalho>Data Retirado</ColunaCabecalho>
-      </LinhaCabecalho>
-      {encomendas.map((encomenda, index) => (
-        <Linha
-          key={index}
-          onClick={() => selecionarLinha(index)}
-          selecionada={linhaSelecionada === index}
+      : undefined,
+    muiTableContainerProps: {
+      sx: {
+        minHeight: '500px',
+      },
+    },
+    onCreatingRowCancel: () => setValidationErrors({}),
+    onCreatingRowSave: handleCreateUser,
+    onEditingRowCancel: () => setValidationErrors({}),
+    onEditingRowSave: handleSaveUser,
+    //optionally customize modal content
+    renderCreateRowDialogContent: ({ table, row, internalEditComponents }) => (
+      <>
+        <DialogTitle variant="h3">Cadastrar Encomenda</DialogTitle>
+        <DialogContent
+          sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
         >
-          <Coluna>{encomenda.id}</Coluna>
-          <Coluna>{encomenda.destinatario}</Coluna>
-          <Coluna>{encomenda.apartamento}</Coluna>
-          <Coluna>{encomenda.bloco}</Coluna>
-          <Coluna>{encomenda.recebido}</Coluna>
-          <Coluna>{encomenda.retirado}</Coluna>
-        </Linha>
-      ))}
-    </DadosContainer>
-  );
+          {internalEditComponents} {/* or render custom edit components here */}
+        </DialogContent>
+        <DialogActions>
+          <MRT_EditActionButtons variant="text" table={table} row={row} />
+        </DialogActions>
+      </>
+    ),
+    //optionally customize modal content
+    renderEditRowDialogContent: ({ table, row, internalEditComponents }) => (
+      <>
+        <DialogTitle variant="h3">Editar Encomenda</DialogTitle>
+        <DialogContent
+          sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
+        >
+          {internalEditComponents} {/* or render custom edit components here */}
+        </DialogContent>
+        <DialogActions>
+          <MRT_EditActionButtons variant="text" table={table} row={row} />
+        </DialogActions>
+      </>
+    ),
+    renderRowActions: ({ row, table }) => (
+      <Box sx={{ display: 'flex', gap: '1rem' }}>
+        <Tooltip title="Edit">
+          <IconButton onClick={() => table.setEditingRow(row)}>
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Delete">
+          <IconButton color="error" onClick={() => openDeleteConfirmModal(row)}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    ),
+    renderTopToolbarCustomActions: ({ table }) => (
+      <Button
+        variant="contained"
+        onClick={() => {
+          table.setCreatingRow(true); //simplest way to open the create row modal with no default values
+          //or you can pass in a row object to set default values with the `createRow` helper function
+          // table.setCreatingRow(
+          //   createRow(table, {
+          //     //optionally pass in default values for the new row, useful for nested data or other complex scenarios
+          //   }),
+          // );
+        }}
+      >
+        Cadastrar Encomenda
+      </Button>
+    ),
+    telefoneCelular: {
+      isLoading: isLoadingUsers,
+      isSaving: isCreatingUser || isUpdatingUser || isDeletingUser,
+      showAlertBanner: isLoadingUsersError,
+      showProgressBars: isFetchingUsers,
+    },
+  });
+
+  return <MaterialReactTable table={table} />;
+};
+
+//CREATE hook (post new user to api)
+function useCreateUser() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (user) => {
+      //send api update request here
+      await new Promise((resolve) => setTimeout(resolve, 1000)); //fake api call
+      return Promise.resolve();
+    },
+    //client side optimistic update
+    onMutate: (newUserInfo) => {
+      queryClient.setQueryData(['users'], (prevUsers) => [
+        ...prevUsers,
+        {
+          ...newUserInfo,
+          id: (Math.random() + 1).toString(36).substring(7),
+        },
+      ]);
+    },
+    // onSettled: () => queryClient.invalidateQueries({ queryKey: ['users'] }), //refetch users after mutation, disabled for demo
+  });
 }
 
-function Paginacao({ totalPaginas, paginaAtual, mudarPagina }) {
-  const handleSkipEsquerda = () => {
-    mudarPagina(1);
-  };
-
-  const handleSkipDireita = () => {
-    mudarPagina(totalPaginas);
-  };
-
-  const handleNumeroPagina = (numero) => {
-    mudarPagina(numero);
-  };
-
-  let inicio = Math.max(1, paginaAtual - 2);
-  let fim = Math.min(totalPaginas, inicio + 4);
-  inicio = Math.max(1, fim - 4);
-
-  return (
-    <PaginacaoContainer>
-      <BotaoSkip onClick={handleSkipEsquerda}>
-        <IoIosArrowBack />
-      </BotaoSkip>
-      {[...Array(fim - inicio + 1).keys()].map((offset) => {
-        const numeroPagina = inicio + offset;
-        return (
-          <NumeroPagina
-            key={numeroPagina}
-            onClick={() => handleNumeroPagina(numeroPagina)}
-            ativo={paginaAtual === numeroPagina}
-          >
-            {numeroPagina}
-          </NumeroPagina>
-        );
-      })}
-      <BotaoSkip onClick={handleSkipDireita}>
-        <IoIosArrowForward />
-      </BotaoSkip>
-    </PaginacaoContainer>
-  );
+//READ hook (get users from api)
+function useGetUsers() {
+  return useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      //send api request here
+      await new Promise((resolve) => setTimeout(resolve, 1000)); //fake api call
+      return Promise.resolve(Dados);
+    },
+    refetchOnWindowFocus: false,
+  });
 }
 
-export default TabelaEncomenda;
+//UPDATE hook (put user in api)
+function useUpdateUser() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (user) => {
+      //send api update request here
+      await new Promise((resolve) => setTimeout(resolve, 1000)); //fake api call
+      return Promise.resolve();
+    },
+    //client side optimistic update
+    onMutate: (newUserInfo) => {
+      queryClient.setQueryData(['users'], (prevUsers) =>
+        prevUsers?.map((prevUser) =>
+          prevUser.id === newUserInfo.id ? newUserInfo : prevUser,
+        ),
+      );
+    },
+    // onSettled: () => queryClient.invalidateQueries({ queryKey: ['users'] }), //refetch users after mutation, disabled for demo
+  });
+}
+
+//DELETE hook (delete user in api)
+function useDeleteUser() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (userId) => {
+      //send api update request here
+      await new Promise((resolve) => setTimeout(resolve, 1000)); //fake api call
+      return Promise.resolve();
+    },
+    //client side optimistic update
+    onMutate: (userId) => {
+      queryClient.setQueryData(['users'], (prevUsers) =>
+        prevUsers?.filter((user) => user.id !== userId),
+      );
+    },
+    // onSettled: () => queryClient.invalidateQueries({ queryKey: ['users'] }), //refetch users after mutation, disabled for demo
+  });
+}
+
+const queryClient = new QueryClient();
+
+const ExampleWithProviders = () => (
+  //Put this with your other react-query providers near root of your app
+  <QueryClientProvider client={queryClient}>
+    <TableApartamentos />
+  </QueryClientProvider>
+);
+
+export default ExampleWithProviders;
+
+const validateRequired = (value) => !!value.length;
+const validateDate = (date) => {
+  const today = dayjs().startOf('day').format('YYYY-MM-DD');
+  return validateRequired(date) && date <= today;
+};
+
+function validateUser(user) {
+  const errors = {};
+
+  // Validação do nome
+  if (!validateRequired(user.nome)) {
+    errors.nome = 'Preencha o campo obrigatório';
+  }
+
+  // Validação da data recebida
+  if (!validateRequired(user.dataRecebido)) {
+    errors.dataRecebido = 'Preencha o campo obrigatório';
+  } else if (!validateDate(user.dataRecebido)) {
+    errors.dataRecebido = 'A data não pode ser maior que a data atual';
+    console.log('Erro de validação: Data inserida maior que a data atual');
+  }
+
+  return errors;
+}
+
