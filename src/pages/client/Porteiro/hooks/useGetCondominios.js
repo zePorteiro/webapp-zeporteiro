b16f6a29b92f3ec3c5b6c4024fda1533e121d437
JuliaHown <juliahown87@gmail.com>
2024-09-17 16:07:28 -0300

    feat: requisicao de busca de condominios

---
+++ src/pages/client/Porteiro/hooks/useGetCondominios.js
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+export const useGetCondominios = () => {
+  return useQuery({
+    queryKey: ['condominios'],
+    queryFn: async () => {
+      const token = sessionStorage.getItem('token');
+      
+      try {
+        const response = await axios.get('/condominios', {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+        return response.data;
+      } catch (error) {
+        console.error('Erro ao obter condomínios:', error.response ? error.response.data : error.message);
+        throw error;
+      }
+    },
+    refetchOnWindowFocus: false,
+    onError: (error) => {
+      console.error('Erro ao obter condomínios:', error);
+    }
+  });
+};
